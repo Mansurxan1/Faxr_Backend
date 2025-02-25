859c672cd363b77fd3545486b04c754ef9a9ede0
Shahzod <sibragimov999@gmail.com>
2025-02-25 15:17:03 +0500

    fix

--- app.js
+++ app.js
@@ -8,7 +8,11 @@ import authRoutes from './routes/authRoutes.js';
 const app = express();
 
 // CORS настройка
-const allowedOrigins = ["http://localhost:5173", "http://localhost:5174"];
+const allowedOrigins = [
+  "http://localhost:5173", 
+  "http://localhost:5174",
+  "https://your-frontend-domain.vercel.app"
+];
 app.use(
   cors({
     origin: allowedOrigins,
